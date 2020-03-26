abfb98d9fc12e0ec1c0f73ea12fc8c2ee9210067
Rémy HUBSCHER <hubscher.remy@gmail.com>
2020-03-26 17:36:04 +0100

    Initial commit.

---
+++ pre_commit_hooks/check_file_change.py
@@ -0,0 +1,56 @@
+"""
+Check that passed filenames have got the expected checksum.
+"""
+import argparse
+import hashlib
+from typing import Optional
+from typing import Sequence
+
+PASS = 0
+FAIL = 1
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--algorithm', default='sha1', help='Algorithm')
+    parser.add_argument('filenames', nargs='+', help='Files to check')
+    args = parser.parse_args(argv)
+
+    files = {}
+    for arg in args.filenames:
+        filename, checksum = arg.split(':')
+        files[filename] = checksum
+
+    retv = PASS
+
+    algorithm = getattr(hashlib, args.algorithm)
+
+    for filename, checksum in files.items():
+        with open(filename, 'rb') as file_obj:
+            digest = algorithm(file_obj.read()).hexdigest()
+
+            if digest != checksum:
+                retv = FAIL
+                print(
+                    f'{bcolors.WARNING}{filename} {algorithm} checksum failed.'
+                    f'{bcolors.ENDC} '
+                    f'Got {bcolors.FAIL}{digest}{bcolors.ENDC} instead of '
+                    f'{bcolors.OKGREEN}{checksum}{bcolors.ENDC}',
+                )
+
+    return retv
+
+
+if __name__ == '__main__':
+    exit(main())
